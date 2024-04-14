302bf009d649333192ebca9b7b31d604bd8df2c9
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-04-14 19:11:52 +0530

    while loop in java

---
+++ loops/While_loop1.java
@@ -0,0 +1,10 @@
+public class While_loop1{
+    public static void main(String args[]){
+        int counter = 0;
+        while (counter < 10){
+            System.out.println("AMAN TIWARI");
+            counter++;
+        }
+    }
+
+}
