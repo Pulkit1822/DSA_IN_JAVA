68591e2b79576386b916abe0297f7a61a8eb157d
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-04-15 18:40:59 +0530

    Add files via upload

---
+++ loops/While_loop2.java
@@ -0,0 +1,11 @@
+public class While_loop2{
+    public static void main(String args[]){
+        int counter = 1;
+        while (counter <= 10){
+            System.out.print(counter+" ");
+            counter++;
+        }
+        
+    }
+
+}
