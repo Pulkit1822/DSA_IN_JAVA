f74f6e04310df7f1863ca74bd0bff6c278aea7a2
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-04-16 21:33:04 +0530

    Add files via upload

---
+++ loops/While_loop3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class While_loop3{
+    public static void main(String srgs[]){
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int counter = 1;
+        
+        while(counter<= range){     //here counter sets to 1 and incresing every while loop until its equal to range.
+            System.out.print(counter + " ");
+            counter++;
+        }
+    }
+}
