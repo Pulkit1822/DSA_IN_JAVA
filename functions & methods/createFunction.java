cdc19e2f4f0829c6c6fec706d4ef5e39c249132a
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-04-30 22:12:52 +0530

    Add files via upload

---
+++ functions & methods/createFunction.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class createFunction {
+    public static void printHelloworld(){     //created our own function to use again & again
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+
+    public static void main(String args[]){   // entry point 
+
+        printHelloworld();       // function call
+    }
+    
+}
